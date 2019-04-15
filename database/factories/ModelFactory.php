36e58b78502ac335232e08128d704730b3717613
orionheidi <mikimojamacka1983>
2019-04-15 21:56:59 +0200

    cors

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,7 +22,7 @@ $factory->define(User::class, function (Faker $faker) {
         'first_name' => $faker->firstName(),
         'last_name' => $faker->lastName(),
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => bcrypt('secret'),
         'remember_token' => Str::random(10),
     ];
 });
